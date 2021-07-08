aee4a08a5ea3b3bf315db749ba2d688950c60173
Alexander Golin <agolin95@gmail.com>
2021-07-08 18:49:53 -0400

    link to empty pages

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("#find").click(function() {
+        alert("find your own home dork!");
+    });
+
+    $("#contact").click(function() {
+        alert("don't you dare try to contact me again.");
+    });
+
+    $("#sell").click(function() {
+        alert("i'll give you two sheckles for it");
+    });
+});
